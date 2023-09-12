9a3d8d3c82eef46cdd01f47923da019092251438
seanzki143 <seanzki143@gmail.com>
2023-09-13 04:55:07 +0800

    Button Fix

--- src/App.js
+++ src/App.js
@@ -60,17 +60,17 @@ function App() {
           </p>
         </div>
         <div className='flex justify-center -mt-8'>
-          <button onClick={generateJoke} disabled={loading} className={`bg-amber-400 text-red-800 leading-0 text-xl font-bold px-8 py-4 rounded-2xl shadow hover:scale-105 transition-all duration-100 animate__animated ${isAnimating ? 'animate__headShake' : 'animate__shakeX animate__delay-5s'}`} >
+          <button onClick={generateJoke} disabled={loading} className={`bg-amber-400 text-red-800 leading-0 text-xl font-bold px-8 py-4 rounded-2xl shadow transition-all duration-100 animate__animated ${isAnimating ? 'animate__headShake' : 'animate__shakeX animate__delay-5s hover:bg-amber-300'}`} >
             { loading ? (
               <div className='flex items-center'>
-                <span className='-mb-2'>TICKLE ME, NORRIS</span>
+                <p className='-mb-0.5'>TICKLE ME, NORRIS</p>
                 <svg className="animate-spin ml-3 h-5 w-5 text-red-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
               </div>
             ) : (
-              <span className='-mb-2'>TICKLE ME, NORRIS</span>
+              <p className='-mb-0.5'>TICKLE ME, NORRIS</p>
             )}
           </button>
         </div>
